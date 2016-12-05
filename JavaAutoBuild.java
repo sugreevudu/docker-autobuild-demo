ddc04b5a20ad873d541a60b6685f3ff6e9a6cf77
vinod kumar <vinodlinux91@users.noreply.github.com>
2016-12-05 14:05:39 +0530

    Update JavaAutoBuild.java

--- JavaAutoBuild.java
+++ JavaAutoBuild.java
@@ -3,6 +3,6 @@ public class JavaAutoBuild {
   public static void main(String[] args) {
       System.out.println("Hallo Docker Workshop!");
           System.out.println("Ich wurde am Docker Donnerstag automatisch erstellt!");
-	  System.out.println("Hi ramu Hi Vinod Hi h r u thank u bye tata");
+	  System.out.println("Hi ramu Hi Vinod Hi h r u thank u bye tata!");
 	    }
 	    }
